caf2b8dea3f98f5dfb6cf84f4372bfac51d3eb77
Roberto Heredia Garland <roberto.hg@hotmail.com>
2015-04-06 13:13:50 -0500

    Poner marcador
    Se modifico el codigo para que aparesca en la posicion deseada en el
    mapa.

--- _ubicacion_files/location.js
+++ _ubicacion_files/location.js
@@ -1,12 +1,64 @@
+function obtenerdireccion(coords) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'LatLng' : coords}, function (results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            var address = result[0].address_components;
+            var direccion = "", ciudad = "", distrito = "";
+
+            if (address[0].long_name) {
+                direccion = direccion + address[0].long_name;
+            }
+            if (address[1].long_name) {
+                direccion = direccion + " " + address[1].long_name;
+            }
+            if (address[2].long_name) {
+                distrito = address[2].long_name;
+                direccion = direccion + " " + address[2].long_name;
+            }
+            if (address[3].long_name) {
+                distrito = address[3].long_name;
+                direccion = direccion + " " + address[3].long_name;
+            }
+            if (address[4].long_name) {
+                ciudad = address[4].long_name;
+                direccion = direccion + " " + address[4].long_name;
+            }
+            if (direccion == "") {
+                direccion = results[1];
+            }
+            document.getElementById("inputAddress").value = direccion;
+            document.getElementById("inputDistrict").value = distrito;
+            document.getElementById("inputCity").value = ciudad;
+          }
+
+                     });
+
+    
+}
+
 var FRDLocation = function () {
+    var Marker;
+    
+    var coords;
+   
+    
 };
+
+
+
+
+
+
 
 FRDLocation.set_location = function (position) {
 
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-
-    var coords = new google.maps.LatLng(latitude, longitude);
+    
+    
+     coords = new google.maps.LatLng(latitude, longitude);
 
     var options = {
         zoom                    : 16,
@@ -16,10 +68,46 @@ FRDLocation.set_location = function (position) {
             style: google.maps.NavigationControlStyle.SMALL
         },
         disableDefaultUI        : true,
-        //zoomControl: true,
+       // zoomControl: true,
         mapTypeId               : google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("location-map"), options);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     Marker = new google.maps.Marker({
+        position: coords,
+        map: map
+    });  
+    
+
+    
+    google.maps.event.addListener(map, 'click', function(event) {
+        coords = event.latLng;
+        Marker.setPosition(coords);
+        //document.getElementById("inputAddress").value = coords;
+        obtenerdireccion(coords);
+    });
+
+
+    
+     Marker.setMap(map);
+     obtenerdireccion(coords);
+    
+    
+   
+
+
+
+
 };
 
 jQuery(document).ready(function () {
